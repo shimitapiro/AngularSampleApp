771153feac8427f0c7abd1d690c28aa484304865
gal schwimmer <test@testadfasdfdafafdsafsdfds.com>
2016-02-09 23:40:14 +0200

    Make STS request

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,11 +9,35 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', ['$scope', function($scope) {
+.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
   $scope.UserName = '';
   $scope.Password = '';
   $scope.submit = function() {
-    window.alert("hi!");
+    $http({
+        url:'https://sts.etoro.com/api/v2/login',
+      method:'POST',
+      headers: {'AccountType': 'Real',
+                'applicationVersion': '1',
+                'applicationIdentifier': 'iOSOpenBook',
+                'Content-Type': 'application/json'
+                },
+      data: {
+        Password: $scope.Password,
+        UserLoginIdentifier: $scope.UserName,
+        Username: $scope.UserName
+      }
+    }).then(function successCallback(response) {
+
+        console.log(response.toString());
+      // this callback will be called asynchronously
+      // when the response is available
+    }, function errorCallback(response) {
+      console.log('errro sts');
+
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+    });
+
     if ($scope.text && $scope.Password) {
       $window.alert("Hello " + $scope.UserName);
     }else {
